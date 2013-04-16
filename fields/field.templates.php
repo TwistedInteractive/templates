134e4b4587dba07235db8c7444f7aa65bc858cd5
Giel Berkers <info@gielberkers.com>
2013-04-16 11:01:44 +0200

    Include xhtml namespace

--- fields/field.templates.php
+++ fields/field.templates.php
@@ -353,7 +353,10 @@ Class fieldTemplates extends Field
 				$dst_file = PageManager::resolvePageFileLocation($fields['path'], $fields['handle']);
 
 				$xsl = '<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:stylesheet version="1.0"
+    xmlns="http://www.w3.org/1999/xhtml"
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    
 	<xsl:include href="'.$src_file.'" />
 </xsl:stylesheet>';
 

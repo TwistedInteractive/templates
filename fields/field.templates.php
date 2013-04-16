f127c52833674571ad37ca834a662134f4cc94a7
Giel Berkers <info@gielberkers.com>
2013-04-16 11:53:25 +0200

    added XHTML namespace

--- fields/field.templates.php
+++ fields/field.templates.php
@@ -356,7 +356,7 @@ Class fieldTemplates extends Field
 <xsl:stylesheet version="1.0"
     xmlns="http://www.w3.org/1999/xhtml"
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    
+
 	<xsl:include href="'.$src_file.'" />
 </xsl:stylesheet>';
 
